3f8dd579470dca27c6a221dad0da32be71db64b2
caleb.p93 <ceptheman2@gmail.com>
2024-05-31 19:07:08 -0700

    'final' draft

--- module-2-js-final-project-caleb-perkins.js
+++ module-2-js-final-project-caleb-perkins.js
@@ -4,104 +4,122 @@ class aGamePossibly {
     }
 
     startGame() {
+        document.write("<br><br><br><br>");
+        this.makeChoice();
+    }
+
+    makeChoice() 
+    {
+
         var userInput = 'o';
+        userInput = prompt("Is this a video game? (Y or y for yes and n or N for no)");
 
-        userInput = prompt("Is this a video game? (Y or y for yes and n or N for no)\n");
+        if (userInput == 'y' || userInput == 'Y') 
+        {
+
+            document.write("You say it is a video game.<br>");
+            document.write("Okay, thanks... <br>");
+            this.itIsAVideoGame();
+
+        } 
+        else if (userInput == 'n' || userInput == 'N') 
+        {
+
+            document.write("You say it's not a video <br>");
+            document.write("Hmm, interesting<br>");
+            this.itsNotAVideoGame();
+
+        } 
+        else 
+        {
+
+            document.body.innerHTML = "";
+            new aGamePossibly();
+
+        }
+    }
+
+    
+    itIsAVideoGame() 
+    {
+
+        document.write("&emsp;&emsp;&emsp;Down <br>&emsp;&emsp;Down <br>Down the rabbit hole we go<br>");
+
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;\\&emsp;&emsp;\\<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;\\&emsp;&emsp;\\<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;/&emsp;&emsp;/<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;/&emsp;&emsp;/<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;\\&emsp;&emsp;\\<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;/&emsp;&emsp;/<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;\\&emsp;&emsp;\\<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;/&emsp;&emsp;/<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;\\&emsp;&emsp;\\<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
+        document.write("&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;|&emsp;&emsp;|<br>");
         
-        if ( ( userInput == 'y' ) || ( userInput == 'Y' ) )
-        {
-        console.log("Okay, thanks\n");
-        }
-        else if ( ( userInput == 'n' ) || ( userInput == 'N' ) )
-        {
-        console.log("Hmm, interesting\n");
-        }
-        else
-        {
-        console.log("It doesn't seem you want to find out.\n");
+        document.write("~=~=~=~=~=~=~=~=~=~=~&emsp;&emsp;&nbsp;~~=~=~=~=~=~=~=~=~=~=~<br>");
+        document.write("{&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;(\\(\\ &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;{}<br>");
+        document.write("}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;(=':')&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&hairsp;{}{}<br>");
+        document.write("{&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;(,(\")(\")&emsp;&emsp;&emsp;&emsp;<==K&emsp;&emsp;&emsp;&hairsp;{}{}{}<br>");
+        document.write("~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=<br>");
+
+        this.videoGameEnding();
+
+    } 
+
+    itsNotAVideoGame() 
+    {
+    
+        document.write("So it's not a video game.<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write(".<br>");
+        document.write("&emsp;Okay<br>");
+        document.write("&emsp;&ensp;.<br>");
+        document.write("&emsp;&ensp;.<br>");
+        document.write("&emsp;&ensp;.<br>");
+        this.notAVideoGameEnding();
+
+    }
+    
+
+    videoGameEnding() 
+    {
         
-        /*console.log("You find yourself in a dark forest. You can go north, south, east, or west.");
-        this.state = { location: 'forest' };
-        this.makeChoice();*/
-    }/*
+        document.write("&emsp;&emsp;&emsp;&ensp;&nbsp;Thanks for sharing, here's an easter (bunny) egg!");
 
-    makeChoice() {
-        let choice = prompt("Which direction do you want to go? (north/south/east/west)").toLowerCase();
-        switch (this.state.location) {
-            case 'forest':
-                this.forest(choice);
-                break;
-            case 'cave':
-                this.cave(choice);
-                break;
-            case 'mountain':
-                this.mountain(choice);
-                break;
-            case 'lake':
-                this.lake(choice);
-                break;
-            default:
-                console.log("Invalid location.");
-                this.startGame();
-        }
     }
 
-    forest(choice) {
-        if (choice === 'north') {
-            console.log("You head north and find a cave.");
-            this.state.location = 'cave';
-        } else if (choice === 'south') {
-            console.log("You head south and reach a mountain.");
-            this.state.location = 'mountain';
-        } else if (choice === 'east') {
-            console.log("You head east and discover a lake.");
-            this.state.location = 'lake';
-        } else if (choice === 'west') {
-            console.log("You head west but find nothing of interest. You return to the forest.");
-        } else {
-            console.log("Invalid choice. Try again.");
-        }
-        this.makeChoice();
-    }
+    notAVideoGameEnding() 
+    {
 
-    cave(choice) {
-        if (choice === 'south') {
-            console.log("You leave the cave and return to the forest.");
-            this.state.location = 'forest';
-        } else {
-            console.log("The cave is dark and scary. You decide to go back.");
-        }
-        this.makeChoice();
-    }
+        document.write("Get refreshed and pop back in<br>");
 
-    mountain(choice) {
-        if (choice === 'north') {
-            console.log("You climb the mountain and find a treasure chest!");
-            console.log("Congratulations, you've won the game!");
-            this.endGame();
-        } else if (choice === 'north') {
-            console.log("You can't go that way. Try another direction.");
-        } else {
-            console.log("You decide to head back to the forest.");
-            this.state.location = 'forest';
-        }
-        this.makeChoice();
     }
-
-    lake(choice) {
-        if (choice === 'west') {
-            console.log("You leave the lake and return to the forest.");
-            this.state.location = 'forest';
-        } else {
-            console.log("The lake is peaceful, but you can't stay here forever. You go back.");
-        }
-        this.makeChoice();
-    }
-
-    endGame() {
-        console.log("Thanks for playing the Adventure Game!");
-    }*/
 }
-}
+
 // Start the game
 new aGamePossibly();
